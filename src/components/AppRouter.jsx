8bcf244a5c7aeb1148f9f08a3071876955432a9b
DimaDimaD <dimaecs@gmail.com>
2023-01-01 16:18:04 +0500

    added router

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,35 +4,41 @@ import About from "../pages/About";
 import Posts from "../pages/Posts";
 import ErrorPage from "../pages/ErrorPage";
 import PostIdPage from "../pages/PostIdPage";
+import {routes} from "../router";
 
 const AppRouter = () => {
     return (
         <div>
             <Routes>
-                <Route
-                    path="/about"
-                    element={<About />}
-                />
-                <Route
-                    exact path="/posts"
-                    element={<Posts />}
-                />
-                <Route
-                    path='/posts/:id'
-                    element={<PostIdPage />}
-                />
-                <Route
-                    path="/error"
-                    element={<ErrorPage />}
-                />
-                <Route
-                    path="/"
-                    element={<Navigate to="/about" />}
-                />
-                <Route
-                    path="*"
-                    element={<Navigate to="/error" />}
-                />
+                {routes.map(route => <Route
+                    path={route.path}
+                    exact={route.exact}
+                    element={<route.element />}
+                    />)}
+                {/*<Route*/}
+                {/*    path="/about"*/}
+                {/*    element={<About />}*/}
+                {/*/>*/}
+                {/*<Route*/}
+                {/*    exact path="/posts"*/}
+                {/*    element={<Posts />}*/}
+                {/*/>*/}
+                {/*<Route*/}
+                {/*    path='/posts/:id'*/}
+                {/*    element={<PostIdPage />}*/}
+                {/*/>*/}
+                {/*<Route*/}
+                {/*    path="/error"*/}
+                {/*    element={<ErrorPage />}*/}
+                {/*/>*/}
+                {/*<Route*/}
+                {/*    path="/"*/}
+                {/*    element={<Navigate to="/about" />}*/}
+                {/*/>*/}
+                {/*<Route*/}
+                {/*    path="*"*/}
+                {/*    element={<Navigate to="/error" />}*/}
+                {/*/>*/}
             </Routes>
         </div>
     );
